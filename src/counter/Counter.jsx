17cd72bf5bccdd7c442f528f56d2156cf687fe13
Hritik897 <pandeyhritik897w@gmail.com>
2024-07-21 22:42:50 +0545

    counter app first draft

---
+++ src/counter/Counter.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    // const [user , setUser]=useState({
+    //   name:  "Enter Your Name" ,
+    //   rollno : "Roll no"
+    // })
+    // const changeName = ()=>{
+    //  setUser ({
+    //       name:  "Hritik Pandy" ,
+    //         rollno : "4650"
+    //  })
+
+    // }
+
+
+    const [value , setValue ]=useState(0);
+
+    const changeValue =()=>{
+        setValue(
+            value+1
+        )
+    }
+
+
+    return (
+
+      
+        
+        <>
+        <h2>Counter App  </h2>
+        <h2>{value}</h2>
+        <button onClick={changeValue}>Increase </button>
+
+     </>
+   )
+ }
+
+export default Counter
